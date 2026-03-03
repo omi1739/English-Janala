4d013cb8fd545861b3fed62f4ff384d3482b73e0
Omi <seyam.islam020@gmail.com>
2026-03-03 22:11:19 +0600

    handle errors and empty lessons for better ux

--- script/index.js
+++ script/index.js
@@ -18,17 +18,29 @@ const displayLevelWords = (words) =>{
    const wordContainer = document.getElementById('word-container');
    wordContainer.innerHTML = '';
 
+   if(words.length == 0){
+    wordContainer.innerHTML = `
+         
+        <div class="text-center py-10  col-span-full rounded-xl space-y-5 font-bangla">
+            <img src="./assets/alert-error.png" class=" mx-auto"/>
+            <p class="text-xl font-medium text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+
+        </div> 
+    `
+   }
+
    words.forEach(word => {
      console.log(word);
      const card = document.createElement('div');
      card.innerHTML = `
         <div class="bg-white rounded-xl shadow-sm text-center px-5 py-10 space-y-4">
-            <h2 class="font-bold text-2xl">${word.word}</h2>
-            <p class="font-semibold">Meaning / Pronounciation</p>
-            <div class="text-2xl font-medium font-bangla">${word.meaning} / ${word.pronunciation}</div>
+            <h2 class="font-bold text-2xl">${word.word ? word.word : "No words available"}</h2>
+            <p class="font-semibold">Meaning / Pronunciation</p>
+            <div class="text-2xl font-medium font-bangla">${word.meaning ? word.meaning : "No meaning available"} / ${word.pronunciation ? word.pronunciation : "No pronunciation available"}</div>
             <div class="flex justify-between items-center">
-                <button class="bg-sky-100 btn hover:bg-sky-300"><i class="fa-solid fa-circle-info"></i></button>
-                <button class="bg-sky-100 btn hover:bg-sky-300"><i class="fa-solid fa-volume-high"></i></button>
+                <button class="bg-sky-50 btn hover:bg-sky-300"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="bg-sky-50 btn hover:bg-sky-300"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>
      `
